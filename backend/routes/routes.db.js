bb658e8d9e18e1f5d88915c40a4a300d8cb67bab
Sidarth V <sidarth.v14002@gmail.com>
2023-07-01 18:57:17 +0530

    rollback: init

--- backend/routes/routes.db.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const db = require("../scripts/db");
-
-router.get("/populate", db.populate);
-router.get("/clear", db.clear);
-
-module.exports = router;
--- backend/scripts/db.js
+++
@@ -1,5 +0,0 @@
-const connect = require("./db.connect");
-const populate = require("./db.populate");
-const clear = require("./db.clear");
-
-module.exports = { connect, populate, clear };
